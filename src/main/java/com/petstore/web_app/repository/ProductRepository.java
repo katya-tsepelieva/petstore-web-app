0bcd97e0e52e0300eb8970ab659e449f4aba47c6
katya-tsepelieva <katyatsepelieva@gmail.com>
2024-11-25 22:27:49 +0200

    Add integration test

--- src/main/java/com/petstore/web_app/repository/ProductRepository.java
+++ src/main/java/com/petstore/web_app/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.petstore.web_app.repository;
 import com.petstore.web_app.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(String category);
 }
